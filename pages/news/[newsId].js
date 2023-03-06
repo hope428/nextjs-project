556ecfb4ddba0865efd0b366ed419c7081ddcbcd
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-06 15:56:51 -0500

    added main layout component

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -3,6 +3,12 @@ import {useRouter} from 'next/router'
 function DetailPage(){
     const router = useRouter()
 
+    //grabs dynamic url param for this page
+    const newsId = router.query.newsId;
+
+    // send request to backend api
+    // fetch news item with newsId
+
     return <h1>The Detail Page</h1>
 }
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,16 @@
- 
+import { Fragment } from "react";
+import Link from "next/link"
 
-export default function NewsPage(){
-    return <h1>The News Page</h1>
+function NewsPage() {
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li><Link href="news/nextjs-is-great">NextJS is a great framework</Link></li>
+        <li><Link href="news/something-else">Something Else</Link></li>
+      </ul>
+    </Fragment>
+  );
 }
+
+export default NewsPage
