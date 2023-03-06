f94feb2fd4c64c00797b0b598d96dc0d772a9069
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-06 17:57:26 -0500

    created meetup details page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+function MeetupDetails(){
+    return (
+        <MeetupDetail image="https://images.unsplash.com/photo-1563876141153-d6d4b0795668?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80" title="First Meetup" address="Berlin, Germany" description="The very first meetup"/>
+    );
+}
+
+export default MeetupDetails
--- pages/index.js
+++ pages/index.js
@@ -3,35 +3,36 @@ import MeetupList from "@/components/meetups/MeetupList";
 import { Fragment } from "react";
 
 function HomePage() {
-
-    const DUMMY_MEETUPS = [
-        {
-            id: 'm1',
-            title: 'A First Meetup',
-            image: 'https://images.unsplash.com/photo-1467269204594-9661b134dd2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-            address: 'Rothenburg ob der Tauber, Germany',
-            description: 'The very first meetup'
-        },
-        {
-            id: 'm2',
-            title: 'A Second Meetup',
-            image: 'https://images.unsplash.com/photo-1634627219651-70f2c49a5bcd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-            address: 'Berlin, Germany',
-            description: 'The second meetup'
-        },
-        {
-            id: 'm3',
-            title: 'A Third Meetup',
-            image: 'https://images.unsplash.com/photo-1563876141153-d6d4b0795668?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80',
-            address: 'Wolfenbüttel, Germany',
-            description: 'The third meetup'
-        }
-    ]
+  const DUMMY_MEETUPS = [
+    {
+      id: "m1",
+      title: "A First Meetup",
+      image:
+        "https://images.unsplash.com/photo-1467269204594-9661b134dd2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+      address: "Rothenburg ob der Tauber, Germany",
+      description: "The very first meetup",
+    },
+    {
+      id: "m2",
+      title: "A Second Meetup",
+      image:
+        "https://images.unsplash.com/photo-1634627219651-70f2c49a5bcd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      address: "Berlin, Germany",
+      description: "The second meetup",
+    },
+    {
+      id: "m3",
+      title: "A Third Meetup",
+      image:
+        "https://images.unsplash.com/photo-1563876141153-d6d4b0795668?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
+      address: "Wolfenbüttel, Germany",
+      description: "The third meetup",
+    },
+  ];
 
   return (
     <Fragment>
-      <Layout />
-      <MeetupList meetups={DUMMY_MEETUPS}/>
+        <MeetupList meetups={DUMMY_MEETUPS} />
     </Fragment>
   );
 }
