1c0a8c5552cdd5f57788139279b52e3f4cdf4f38
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-07 18:54:49 -0500

    added mongodb connection for individual meetup page to pull mongodb information based on the id in the url

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,47 +1,42 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
+import { MongoClient } from "mongodb";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://images.unsplash.com/photo-1563876141153-d6d4b0795668?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-      title="First Meetup"
-      address="Berlin, Germany"
-      description="The very first meetup"
+      image={props.image}
+      title={props.title}
+      address={props.address}
+      description={props.description}
     />
   );
 }
 
 export async function getStaticPaths() {
+
+    const client = await MongoClient.connect(process.env.MONGODB_URI)
+    const meetupCollection = client.db().collections('meetups')
+
+    const meetups = await meetupCollection.find({}, {_id: 1}).toArray()
+    client.close()
+
   return {
-    fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    fallback: true,
+    paths: meetups.map(meetup => ({params: {meetupId: meetup._id.toString()}}))
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
+  const client = await MongoClient.connect(process.env.MONGODB_URI)
+
+  const meetupCollection = client.db().collections('meetups')
+  const currentMeetup = await meetupCollection.findOne({_id: meetupId})
+
   return {
     props: {
-      meetupData: {
-        id: meetupId,
-        image:
-          "https://images.unsplash.com/photo-1563876141153-d6d4b0795668?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
-        title: "First Meetup",
-        address: "Berlin, Germany",
-        description: "The very first meetup",
-      },
+      meetupData: currentMeetup
     },
   };
 }
