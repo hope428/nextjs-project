f177dcbdbef8c9d58076a031fdce20e9c2425fd2
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-07 16:39:57 -0500

    in index.js, replaced dummy data with data from mongo atlast database

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,6 @@
 import MeetupList from "@/components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
 
-const DUMMY_MEETUPS = [
-    {
-      id: "m1",
-      title: "A First Meetup",
-      image:
-        "https://images.unsplash.com/photo-1467269204594-9661b134dd2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-      address: "Rothenburg ob der Tauber, Germany",
-      description: "The very first meetup",
-    },
-    {
-      id: "m2",
-      title: "A Second Meetup",
-      image:
-        "https://images.unsplash.com/photo-1634627219651-70f2c49a5bcd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-      address: "Berlin, Germany",
-      description: "The second meetup",
-    },
-    {
-      id: "m3",
-      title: "A Third Meetup",
-      image:
-        "https://images.unsplash.com/photo-1563876141153-d6d4b0795668?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
-      address: "Wolfenbüttel, Germany",
-      description: "The third meetup",
-    },
-  ];
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
@@ -33,9 +8,24 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   //fetch data from an api or database
+
+  const client = await MongoClient.connect('mongodb+srv://leon-forsythe:hinata11!@cluster0.znbosnr.mongodb.net/reactMeetups?retryWrites=true&w=majority')
+
+  const db = client.db()
+  const meetupCollections = db.collection('meetups')
+
+  const result = await meetupCollections.find().toArray()
+
+  console.log(result);
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: result.map(meetup => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString()
+      })),
     },
     revalidate: 10
   };
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,6 +1,10 @@
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 function NewMeetup() {
+
+  const router = useRouter()
+
   async function addMeetupHandler(enteredMeetupData) {
     const response = await fetch('/api/new-meetup', {
       method: 'POST',
@@ -11,8 +15,7 @@ function NewMeetup() {
     })
 
     const data = await response.json()
-    console.log(data);
-
+    router.push('/')
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
